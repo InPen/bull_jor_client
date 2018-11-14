736be0ab6568d3f8d063f9f884e392f67044bbfc
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-11-14 18:54:41 -0500

    Add first working app

---
+++ journal.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+# web server
+app = Flask(__name__)
+# first route
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+# take app variable and call method run
+if __name__=='__main__':
+    app.run(debug=True)
+# to run server run `python <path of file>`
