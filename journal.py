524e1ce603f205c1f09cc35fb9c4e6936682e8ec
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-11-14 19:11:41 -0500

    Add `to_do` route

--- journal.py
+++ journal.py
@@ -10,6 +10,10 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html')
+# third route
+@app.route('/to_do')
+def to_do():
+    return render_template('to_do.html')
 # take app variable and call method run
 if __name__=='__main__':
     app.run(debug=True)
